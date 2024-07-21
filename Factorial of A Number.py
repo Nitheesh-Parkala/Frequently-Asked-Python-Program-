fb078f532dee09d71bb15bb592b7d2c483928d5c
Nitheesh-Parkala <admin@DESKTOP-DM51N1K>
2024-07-21 11:50:13 +0530

    Factorial of A Number

---
+++ Factorial of A Number.py
@@ -0,0 +1,15 @@
+# 5!=1*2*3*4*5
+
+factorial=1
+num =26
+i=1
+if(num<0):
+    print("Factorial is not exits for a negative number")
+elif(num==0):
+    print("Factorial of 0 is 1")
+else:
+    # for i in range(1,num+1):
+    while(i<=num):
+        factorial = factorial * i
+        i+=1
+    print("The Factorial of",num,"is",factorial)
